ce79347cf32f3e88a28f0e2430eddf0ba9e7ee6a
MarkoMH2004 <markmhmamani@gmail.com>
2026-09-23 23:28:54 -0500

    Delete RedBlue directory

--- RedBlue/src/main/java/pe/edu/upc/demosi/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.demosi.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- RedBlue/src/main/java/pe/edu/upc/demosi/repositories/IRolRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.demosi.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.demosi.entities.Rol;
-
-@Repository
-public interface IRolRepository extends JpaRepository<Rol, Long> {
-}
